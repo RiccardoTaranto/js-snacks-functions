a7444231727e4635f1fa147ad5b86533a7d852b5
Riccardo <riccardo.taranto@icloud.com>
2026-03-26 12:26:20 +0100

    conta_vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,19 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function getVowels(string){ 
+let counter = 0;
+for (let i =0; i<string.length; i++){
+ if (string[i].includes('a') || string[i].includes('e') || string[i].includes('i') || string[i].includes('o') || string[i].includes('u')) {
+ counter++; 
+}
+}
+return counter;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const filteredVowels = getVowels(word);
+console.log(filteredVowels);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
